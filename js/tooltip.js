1a0c0bb0e76424a506f3c4467c27c4712e2c3820
Nataliia Jefimockina <natik.rusakova@gmail.com>
2017-03-30 22:17:48 +0300

    Edited tooltip-files

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,21 +1,21 @@
-$(function () {
-    $('.helptip').hide();
+var title;
+$('input').hover(function() {
+    title = $(this).attr('title');
+    $(this).attr('title', '');
+    $(this).parent('label').append("<span class='title'>"+title+"</span>")
+},
 
-    $('[data-tooltip]').on('mouseenter', function (eventObject) {
-        var $dataTooltip = $(this).attr('data-tooltip');
-        var $helpId = $(this).attr('id');
-        $('.' + $helpId).text($dataTooltip).fadeIn()
-    });
+function() {
+    $('.title').remove();
+    $(this).attr('title', title);
+});
 
-    $('[data-tooltip]').on('mouseleave', function () {
-        $('.helptip').hide().text('')
-    });
-
-    $('.help').on('click',function() {
-        $('[data-tooltip]').each(function() {
-            var $dataTooltip = $(this).attr('data-tooltip');
-            var $helpId = $(this).attr('id');
-            $('.' + $helpId).text($dataTooltip).fadeIn()
-        });
-    });
+$("form").on('submit', function(e) {
+    e.preventDefault();
+    $('.title').remove()
+    $('input').each(function () {
+        var title = $(this).attr('title');
+        $(this).parent('label').append("<span class='title'>"+title+"</span>");
+      });
+      return false;
 });
